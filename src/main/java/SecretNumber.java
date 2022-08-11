c227704ba730d8d47e2597c76eea2c428a0b55d5
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-11 17:53:07 +0100

    added a scanner object to request and collect user input

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -8,7 +8,12 @@ public class SecretNumber {
         int secretNumber = 64;
 
 //        request a user to input a number
+        System.out.println("Guess a secret number:");
+        Scanner reader = new Scanner(System.in); // creating a scanner object
+
 //        collect user input
+        int guessNumber = Integer.parseInt(reader.nextLine());
+
 //        check if the user input matches the secret number:
 //          report if the incorrect guess was too high
 //          report if the incorrect guess was too low
