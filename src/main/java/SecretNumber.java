1ab8bebba8a2c261ee7dcbaf1b8678bbad7be1df
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-11 17:47:10 +0100

    devised pseudo code for the guessing game

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
 
+//        create a variable with a secret number
         int secretNumber = 64;
-        
+
+//        request a user to input a number
+//        collect user input
+//        check if the user input matches the secret number:
+//          report if the incorrect guess was too high
+//          report if the incorrect guess was too low
+//          report if the user guessed the number correctly
+
     }
 
 }
