383361b8312467a27e56dca5315965071f78daca
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-11 18:21:37 +0100

    devised pseudo code for returning clothing suggestions

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -2,6 +2,17 @@ public class WhatToWear {
 
     public static void main(String[] args) {
 
+//        request user input about the weather
+//        collect weather input
+//        request user input about the average temperature
+//        collect temperature input
+//        output clothing suggestions:
+//          if rainy & temperature < 12
+//          if sunny & temperature > 20
+//          if snowy & temperature < 0
+//          if cloudy & temperature < 15
+
+
     }
 
 }
