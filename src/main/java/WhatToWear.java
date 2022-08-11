65ebb00a7c4a6ae0f007c2e05e54fde24aa44b0b
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-08-11 18:27:00 +0100

    added user input requests for weather and temperature

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class WhatToWear {
 
     public static void main(String[] args) {
 
 //        request user input about the weather
+        System.out.println("Describe the weather:");
+        Scanner reader = new Scanner(System.in);
 //        collect weather input
+        String weather = reader.nextLine();
+
 //        request user input about the average temperature
+        System.out.println("What is the average temperature for today?");
+        Scanner reader2 = new Scanner(System.in);
 //        collect temperature input
+        int temperature = Integer.parseInt(reader.nextLine());
+
 //        output clothing suggestions:
 //          if rainy & temperature < 12
 //          if sunny & temperature > 20
